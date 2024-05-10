bd81164b0dee630e2259f48cac731645b3497364
Curtis Burchett <curtis@burchett.me>
2024-05-10 20:40:32 +0000

    Working on Module 0

--- Exercise 0/exercise0Task4.sh
+++ Exercise 0/exercise0Task4.sh
@@ -6,12 +6,9 @@
 DIR='/home/user/.kube' 
 sudo apt install sshpass
 sshpass -p Netapp1! scp -o "StrictHostKeyChecking=no" root@kubmas1-1:/root/.kube/config config1 
-sshpass -p Netapp1! scp -o "StrictHostKeyChecking=no" root@kubmas2-1:/root/.kube/config config2 
-sudo sed -i 's/\<kubernetes\>/kubernetes2/g' config2
-konfig=$(KUBECONFIG=config1:config2 kubectl config view --flatten)
+konfig=$(KUBECONFIG=config1 kubectl config view --flatten)
 if [ ! -d "$DIR" ]; then
     mkdir -p $DIR;
 fi
 echo "$konfig" > $DIR/config
-#echo "$konfig" > config
-$KUBECONFIG=$DIR/config
+export KUBECONFIG=$DIR/config
